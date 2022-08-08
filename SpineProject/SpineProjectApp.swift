6a1c1ca692298629c17371f75c62c80676f8d9b4
ailidhk11 <ailidhkinney@gmail.com>
2022-08-08 20:40:19 +0100

    Initial Commit

---
+++ SpineProject/SpineProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpineProjectApp.swift
+//  SpineProject
+//
+//  Created by Ailidh Kinney on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpineProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
